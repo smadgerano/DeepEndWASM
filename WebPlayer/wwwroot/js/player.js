aa4571480ed29f0468999370ef4877208807dae2
AndyN <a.nic@live.com>
2022-04-08 17:19:42 +0100

    Fade tweaks

--- WebPlayer/wwwroot/js/player.js
+++ WebPlayer/wwwroot/js/player.js
@@ -3,21 +3,26 @@
     html5: true
 });
 
+
+
 function startHowlerPlayer() {
-    sound.play();
-    sound.fade(0, 1, 1000);
+    var id1 = sound.play();
+    sound.fade(0, 1, 1000, id1);
 }
 
 function stopHowlerPlayer() {
-    sound.fade(1, 0, 1000);
+    sound.fade(1, 0, 1000, id1);
+    await sleep(1000);
     sound.stop();
 }
 
 function switchHowlerStream(url) {
-    sound.fade(1, 0, 1000);
+    sound.fade(1, 0, 1000, id1);
+    await sleep(1000);
     sound.stop();
     sound.unload();
     sound.src = url;
-    sound.play();
-    sound.fade(0, 1, 1000);
+    var id1 = sound.play();
+    sound.fade(0, 1, 1000, id1);
 }
+
