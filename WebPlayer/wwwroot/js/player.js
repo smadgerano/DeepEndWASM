159ec12d50059f9337e0d4ceba7adf430181c6ea
AndyN <a.nic@live.com>
2022-04-08 09:12:29 +0100

    Added fades and updated player UI

--- WebPlayer/wwwroot/js/player.js
+++ WebPlayer/wwwroot/js/player.js
@@ -5,15 +5,19 @@
 
 function startHowlerPlayer() {
     sound.play();
+    sound.fade(0, 1, 1000);
 }
 
 function stopHowlerPlayer() {
+    sound.fade(1, 0, 1000);
     sound.stop();
 }
 
 function switchHowlerStream(url) {
+    sound.fade(1, 0, 1000);
     sound.stop();
     sound.unload();
     sound.src = url;
     sound.play();
+    sound.fade(0, 1, 1000);
 }
