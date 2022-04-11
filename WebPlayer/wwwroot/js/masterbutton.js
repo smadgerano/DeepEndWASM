755261f0585ed38e0406e21a82b5bda171083edc
AndyN <a.nic@live.com>
2022-04-11 10:03:48 +0100

    Updates

--- WebPlayer/wwwroot/js/masterbutton.js
+++ WebPlayer/wwwroot/js/masterbutton.js
@@ -1,14 +1,15 @@
 ﻿export async function startPlayer(url) {
     var stream = document.getElementById('streamSourceElement');
     stream.src = url;
-    await new Promise(res => { setTimeout(res, 10); });
+    //await new Promise(res => { setTimeout(res, 10); });
     stream.load();
     stream.play();
-}
+};
 
 export async function stopPlayer() {
     var stream = document.getElementById('streamSourceElement');
     stream.pause();
     stream.src = "";
     stream.load();
-}
+};
+
--- WebPlayer/wwwroot/js/webaudio.js
+++ WebPlayer/wwwroot/js/webaudio.js
@@ -1,71 +1,47 @@
-﻿//var context = new (window.AudioContext || window.webkitAudioContext)();
-////var context = new AudioContext();
-////var audioElement = document.getElementById("streamSourceElement");
-//var audioElement = document.querySelector('audio');
-export async function loadWebAudio(){
+﻿export async function loadWebAudio() {
+
     let context = new (window.AudioContext || window.webkitAudioContext)();
-
     let audioElement = document.querySelector('#streamSourceElement');
-
     let source = context.createMediaElementSource(audioElement);
-
     let gainNode = context.createGain();
-    gainNode.gain.value = 0.92;
-
-
-    //var analyser = context.createAnalyser();
-    //analyser.fftSize = 2048;
-    //var bufferLength = analyser.frequencyBinCount;
-    //var dataArray = new Uint8Array(bufferLength);
-    //analyser.getByteTimeDomainData(dataArray);
-
-    //source.connect(analyser);
 
     source.connect(gainNode);
-
     gainNode.connect(context.destination);
 
 
+    //document.getElementById('masterVolume').addEventListener('input', function () {
+    //    gainNode.gain.value = this.value;
 
-    //var canvas = document.getElementById("oscilloscope");
-    //var canvasCtx = canvas.getContext("2d");
+    //    // document.getElementById('volumeSlider').value = this.value;
+    //    //window.WriteCSharpMessageToConsole = (dotnetHelper) => {
+    //    //    dotnetHelper.invokeMethod('UpdateVolumeState', this.value);
+    //    //}
+    //});
 
-    //// draw an oscilloscope of the current audio source
+    //document.getElementById('waapiVolumeSlider').addEventListener('input', function () {
+    //    gainNode.gain.value = this.value;
 
-    //function draw() {
+    ////    // document.getElementById('volumeSlider').value = this.value;
+    ////    //window.WriteCSharpMessageToConsole = (dotnetHelper) => {
+    ////    //    dotnetHelper.invokeMethod('UpdateVolumeState', this.value);
+    //    //}
+    //});
 
-    //    requestAnimationFrame(draw);
 
-    //    analyser.getByteTimeDomainData(dataArray);
+    //document.getElementById('masterButton').addEventListener('click', function () {
+    //    var masterButtonIcon = document.getElementById('masterButtonIcon');
+    //    var stream = document.getElementById('streamSourceElement');
+    //    var source = document.getElementById('streamURL').innerHTML;
 
-    //    canvasCtx.fillStyle = '#21252900';
-    //    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-    //    canvasCtx.lineWidth = 2;
-    //    canvasCtx.strokeStyle = "rgb(0, 0, 0)";
-
-    //    canvasCtx.beginPath();
-
-    //    var sliceWidth = canvas.width * 1.0 / bufferLength;
-    //    var x = 0;
-
-    //    for (var i = 0; i < bufferLength; i++) {
-
-    //        var v = dataArray[i] / 128.0;
-    //        var y = v * canvas.height / 2;
-
-    //        if (i === 0) {
-    //            canvasCtx.moveTo(x, y);
-    //        } else {
-    //            canvasCtx.lineTo(x, y);
-    //        }
-
-    //        x += sliceWidth;
+    //    if (masterButtonIcon.classList.contains('fa-circle-pause')) {           
+    //        stream.pause();
+    //        stream.src = "";
+    //        stream.load();
+    //    } else {
+    //        stream.src = source;
+    //        stream.load();
+    //        stream.play();
     //    }
-
-    //    canvasCtx.lineTo(canvas.width, canvas.height / 2);
-    //    canvasCtx.stroke();
-    //}
-
-    //draw();
+    //});    
 }
+
